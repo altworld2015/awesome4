35a0aef5cdae637a0eb6f7a5acd7b3f7dc15c425
altworld2015 <altworld2015@yandex.ru>
2018-04-15 01:13:56 -0400

    awesome4

--- desktop_icons.lua
+++ desktop_icons.lua
@@ -17,10 +17,10 @@ desktop_wibox:set_widget(my_widget)
 ------------------
 local my_widget1 = wibox.widget.textbox()
 --my_widget1.text = " Firefox "
-my_widget1.markup = "<span color=\"#f29466\"><span font=\"odstemplik 15\">TorBrowser</span></span>"
+my_widget1.markup = "<span color=\"#f29466\"><span font=\"odstemplik 15\"><b>TorBrowser</b></span></span>"
 function my_widget1:fit(context, width, height) return width, height end
 local desktop_wibox1 = wibox({ type = "desktop", visible = true, bg = "#00000000" })
-desktop_wibox1:geometry({ x = 928, y = 100, width = 90, height = 10 })
+desktop_wibox1:geometry({ x = 928, y = 100, width = 90, height = 18 })
 desktop_wibox1:set_widget(my_widget1)
 -------------------
 -------------------------
@@ -34,10 +34,10 @@ desktop_wibox3:geometry({ x = 950, y = 160, width = 32, height = 32 })
 desktop_wibox3:set_widget(my_widget3)
 --
 local my_widget4 = wibox.widget.textbox()
-my_widget4.markup = "<span color=\"#f29466\"><span font=\"odstemplik 15\">File System</span></span>"
+my_widget4.markup = "<span color=\"#f29466\"><span font=\"odstemplik 15\"><b>File System</b></span></span>"
 function my_widget4:fit(context, width, height) return width, height end
 local desktop_wibox4 = wibox({ type = "desktop", visible = true, bg = "#00000000" })
-desktop_wibox4:geometry({ x = 930, y = 200, width = 90, height = 10 })
+desktop_wibox4:geometry({ x = 930, y = 200, width = 90, height = 21 })
 desktop_wibox4:set_widget(my_widget4)
 
 -----------
@@ -52,10 +52,10 @@ desktop_wibox5:geometry({ x = 950, y = 260, width = 32, height = 32 })
 desktop_wibox5:set_widget(my_widget5)
 --
 local my_widget6 = wibox.widget.textbox()
-my_widget6.markup = "<span color=\"#f29466\"><span font=\"odstemplik 15\">Home</span></span>"
+my_widget6.markup = "<span color=\"#f29466\"><span font=\"odstemplik 15\"><b>Home</b></span></span>"
 function my_widget6:fit(context, width, height) return width, height end
 local desktop_wibox6 = wibox({ type = "desktop", visible = true, bg = "#00000000" })
-desktop_wibox6:geometry({ x = 946, y = 300, width = 50, height = 10 })
+desktop_wibox6:geometry({ x = 946, y = 300, width = 50, height = 18 })
 desktop_wibox6:set_widget(my_widget6)
 
 
@@ -71,10 +71,10 @@ desktop_wibox7:geometry({ x = 950, y = 360, width = 32, height = 32 })
 desktop_wibox7:set_widget(my_widget7)
 --
 local my_widget8 = wibox.widget.textbox()
-my_widget8.markup = "<span color=\"#f29466\"><span font=\"odstemplik 15\">Trash</span></span>"
+my_widget8.markup = "<span color=\"#f29466\"><span font=\"odstemplik 15\"><b>Trash</b></span></span>"
 function my_widget8:fit(context, width, height) return width, height end
 local desktop_wibox8 = wibox({ type = "desktop", visible = true, bg = "#00000000" })
-desktop_wibox8:geometry({ x = 948, y = 400, width = 50, height = 10 })
+desktop_wibox8:geometry({ x = 948, y = 400, width = 50, height = 18 })
 desktop_wibox8:set_widget(my_widget8)
 
 
@@ -91,8 +91,8 @@ desktop_wibox9:set_widget(my_widget9)
 --
 local my_widget10 = wibox.widget.textbox()
 --my_widget10.markup = "<span color=\"#f29466\">Acestream</span>"
-my_widget10.markup = "<span color=\"#f29466\"><span font=\"odstemplik 14\">Acestream</span></span>"
+my_widget10.markup = "<span color=\"#f29466\"><span font=\"odstemplik 14\"><b>Acestream</b></span></span>"
 function my_widget10:fit(context, width, height) return width, height end
 local desktop_wibox10 = wibox({ type = "desktop", visible = true, bg = "#00000000" })
-desktop_wibox10:geometry({ x = 938, y = 500, width = 70, height = 10 })
+desktop_wibox10:geometry({ x = 938, y = 500, width = 70, height = 18 })
 desktop_wibox10:set_widget(my_widget10)
